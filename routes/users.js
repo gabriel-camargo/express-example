d18635f37dc65248a999143a30ab2f556556d8a4
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2019-07-29 21:40:25 -0300

    put e delete

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const db = new NeDB({
 module.exports = app => {
 
     let route = app.route('/users');
+    let routeId = app.route('/users/:id');
 
     route.get((req, res) => {
         db.find({}).sort({name:1}).exec( (err, users) => {
@@ -32,5 +33,36 @@ module.exports = app => {
             }
         })
 
+    }); 
+
+    routeId.get( (req, res) => {
+        db.findOne({_id: req.params.id}).exec( (err, user) => {
+            if(err) {
+                app.utils.error.send(err, req, send);
+            } else {
+                res.status(200).json(user);
+            }
+        });
     });
+
+    routeId.put( (req, res) => {
+        db.update({_id: req.params.id}, req.body, err => {
+            if(err) {
+                app.utils.error.send(err, req, send);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        });
+    });
+
+    routeId.delete( (req, res) => {
+        db.remove({_id: req.params.id}, {}, err => {
+            if(err) {
+                app.utils.error.send(err, req, send);
+            } else {
+                res.status(200).json(req.params);
+            }
+        });
+    });
+    
 }
